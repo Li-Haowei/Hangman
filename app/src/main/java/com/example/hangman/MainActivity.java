0a44fa53cc1667ccdd1ba057e275d767b72e9707
Haowei Li <haoweilee666@gmail.com>
2022-02-17 02:26:40 -0500

    GRE words txt file extractor

--- app/src/main/java/com/example/hangman/MainActivity.java
+++ app/src/main/java/com/example/hangman/MainActivity.java
@@ -3,12 +3,41 @@ package com.example.hangman;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-
+    private String[][] GRE = new String[676][];
+    private String currentWord;
+    private String hint;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        try {
+            readFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+    private void readFile() throws IOException {
+        File file = new File("\\value\\words.txt");
+        String st;
+        int i = 0;
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        while ((st = br.readLine()) != null){
+            String[] wordAndHint = extractWord(st);
+            GRE[i] = wordAndHint;
+            i++;
+        }
+    }
+    private String[] extractWord(String line){
+        String[] wordAndHint = line.split(" - ",2);
+        return wordAndHint;
+    }
+
 }
